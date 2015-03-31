d30f78f1b4a1907e28b537f538886ad03f5210b1
Konstantin Kiselev <knkiselyov@84.201.173.136-red2.dhcp.yndx.net>
2015-03-31 13:44:42 +0300

    Add debug output

--- lib/babelyoda/strings_lexer.rb
+++ lib/babelyoda/strings_lexer.rb
@@ -3,10 +3,16 @@ module Babelyoda
     TOKENS = [ :multiline_comment, :singleline_comment, :string, :reserved0, :equal_sign, :semicolon ].freeze
 
     def lex(str)
-      str.scan(/(\/\*.*\*\/)|(\s*\/\/.*\n)|((["])(?:\\?+.)*?\4)|(\s*=\s*)|(;)/).each do |m|
+      rgxp = /(\/\*.*\*\/)|(\s*\/\/.*\n)|((["])(?:\\?+.)*?\4)|(\s*=\s*)|(;)/
+      $logger.debug ">>> Scanning file with lexer ('#{rgxp}'). Tokens:"
+      
+      str.scan(rgxp).each do |m|
+      	$logger.debug "\t\t * #{m}"
+
         idx = m.index { |x| x }
         yield TOKENS[idx], m[idx].strip
       end
+      $logger.debug ">>> Finish scanning"
     end
   end
 end
